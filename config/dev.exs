d6d0434647ff1e0dde79a6dae79fad08e43b8d64
John Kohler <johnkohler@Johns-MacBook-Air.local>
2020-09-24 14:45:35 -0400

    update db

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :jest_service, JestService.Repo,
   username: "postgres",
-  password: "",
+  password: "postgres",
   database: "jest_service_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
